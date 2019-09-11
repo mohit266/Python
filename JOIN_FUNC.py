5b821d955ff67aa81e33bc7ce01cf064f077ff72
mohit266 <16tec2cs012@vgu.ac.in>
2019-09-11 16:04:39 +0530

    join function in python

---
+++ JOIN_FUNC.py
@@ -0,0 +1,10 @@
+list = ["Rohan", "Mohit", "Rahul", "Dhiraj", "David"]
+
+# for i in list:
+#     print(i, "and", end=" ")
+
+# join function
+a = " and ".join(list)
+b = " , ".join(list)
+print(a)
+print(b)
