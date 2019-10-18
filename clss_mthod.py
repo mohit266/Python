8fa697ed01049a37bb6160cd1d1086817a4fe2d6
mohit266 <16tec2cs012@vgu.ac.in>
2019-10-18 23:26:16 +0530

    class method

---
+++ clss_mthod.py
@@ -0,0 +1,21 @@
+class Employee:
+    no_of_leaves = 8
+
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f"The name is {self.name}. Salary is {self.salary} and role is {self.role}"
+
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
+
+    harry = Employee("Harry", 255, "Instructor")
+
+    rohan.change_leaves(34)
+
+
+
