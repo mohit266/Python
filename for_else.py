4df1c918fda6a150bbb913a3462a89e92b8492d1
mohit266 <16tec2cs012@vgu.ac.in>
2019-10-24 13:32:28 +0530

    else conditional statement with for loop

---
+++ for_else.py
@@ -0,0 +1,7 @@
+Cricket = ["Bat", "Ball", "Stumps", "Bails", "Gloves", "Helmet"]
+for item in Cricket:
+    if item == 'Ball':
+        print(item)
+        break
+else:
+    print("Sorry! Your item was not found.")
