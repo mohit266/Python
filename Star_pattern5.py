d95707b6934d197e39ebad60ad2ee927dd88d3a6
mohit266 <16tec2cs012@vgu.ac.in>
2019-11-21 23:55:01 +0530

    Pattern

---
+++ Star_pattern5.py
@@ -0,0 +1,21 @@
+def pattern(n):
+    # for loop for rows
+    for i in range(1, n + 1):
+
+        # conditional operator
+        k = i + 1 if (i % 2 != 0) else i
+
+        # for loop for printing spaces
+        for g in range(k, n):
+            if g >= k:
+                print(end="  ")
+
+        for j in range(0, k):
+            if j == k - 1:
+                print(" * ")
+            else:
+                print(" * ", end=" ")
+
+
+n = 10
+pattern(n)
